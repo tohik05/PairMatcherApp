1a902305a3c99666ff0db8837685a765a991a47e
Anton Khoroshok <tohik05@gmail.com>
2023-10-30 12:00:29 +0200

    removed phantom mark repo

--- src/main/java/com/andersen/orange/marks/repository/MarksRepository.java
+++
@@ -1,9 +0,0 @@
-package com.andersen.orange.marks.repository;
-
-import com.andersen.orange.marks.model.Marks;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MarksRepository extends JpaRepository<Marks, Long> {
-}
