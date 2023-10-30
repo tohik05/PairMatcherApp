c3f32a86551bf59e2f45bfeff4555725fe7ece09
Anton Khoroshok <tohik05@gmail.com>
2023-10-30 11:42:55 +0200

    Changes: -created mark entity -changed logic how to add mark in DB

--- src/main/java/com/andersen/orange/pair/service/PairServiceImpl.java
+++ src/main/java/com/andersen/orange/pair/service/PairServiceImpl.java
@@ -1,5 +1,6 @@
 package com.andersen.orange.pair.service;
 
+import com.andersen.orange.marks.service.MarksService;
 import com.andersen.orange.pair.dto.PairRequestDto;
 import com.andersen.orange.pair.dto.PairResponseDto;
 import com.andersen.orange.pair.model.Pair;
@@ -21,15 +22,17 @@ public class PairServiceImpl implements PairService {
     private final PairMapper pairMapper;
     private final Algorithm algorithm;
     private final UserRepository userRepository;
+    private final MarksService marksService;
 
     @Autowired
     public PairServiceImpl(PairRepository repository, UserMapper userMapper, PairMapper pairMapper, Algorithm algorithm,
-                           UserRepository userRepository) {
+                           UserRepository userRepository, MarksService marksService) {
         this.pairRepository = repository;
         this.userMapper = userMapper;
         this.pairMapper = pairMapper;
         this.algorithm = algorithm;
         this.userRepository = userRepository;
+        this.marksService = marksService;
     }
 
     @Override
@@ -55,15 +58,14 @@ public class PairServiceImpl implements PairService {
         mainPairs.add(pair);
         main.setPairs(mainPairs);
         userRepository.save(main);
+        marksService.saveMark(main, pair, pairDto.getMainUser().getMark());
 
         User opponent = pair.getUsers().get(1);
         List<Pair> opponentPairs = opponent.getPairs();
         opponentPairs.add(pair);
         opponent.setPairs(opponentPairs);
         userRepository.save(opponent);
-
-        pairRepository.insertUserPairMark(main.getId(), pair.getId(), pairDto.getMainUser().getMark());
-        pairRepository.insertUserPairMark(opponent.getId(), pair.getId(), pairDto.getOpponentUser().getMark());
+        marksService.saveMark(opponent,pair, pairDto.getOpponentUser().getMark());
     }
 
 }
